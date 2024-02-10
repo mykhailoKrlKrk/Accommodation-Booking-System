90d7261a2a22f21aa245e341a805ac150e4acd38
Mykhailo Kuryk <113907200+mishaa16@users.noreply.github.com>
2024-02-10 23:01:51 +0200

    edit CI file/fix build

--- accommodation-service/src/main/java/org/accommodation/bookingservice/controller/AccommodationController.java
+++ accommodation-service/src/main/java/org/accommodation/bookingservice/controller/AccommodationController.java
@@ -1,8 +1,5 @@
 package org.accommodation.bookingservice.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
